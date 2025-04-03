76d09c830498fc2411b06f044062e877586a180f
libiaf <a01253425@tec.mx>
2025-04-02 23:40:08 -0700

    Importar router a index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from "express";
+import apiRouter from './src/routes';
 
-const app:Express = express();
-const morgan = require('morgan')
-
+const app: Express = express();
+const morgan = require('morgan');
 const port = 3000;
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(express.json()); 
+app.use(apiRouter);
 
-app.get('/', (req:Request, res:Response) => { 
-   res.send('Hello Typescript!') 
-}) 
-app.listen(port, () => { 
-   console.log(`Example app listening on port ${port}`) 
-}) 
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`);
+});
+
 
